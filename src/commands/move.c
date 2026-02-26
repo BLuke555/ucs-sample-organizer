4dea2c0c9f14fd7f957ded8b8b5a1fcb3b26113b
BLuke555 <lucacenci99@gmail.com>
2026-02-26 20:02:31 +0100

    added move funciton

--- src/commands/move.c
+++ src/commands/move.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "move.h"
 
 
 void move(string8 args[4]) {
-	printf("moving shit at %s\n", args[1].str);
+	if (!args[1].str || !args[2].str) {
+		printf("error: the move funciton wants 2 arguments");
+		return;
+	}
+
+	if (rename(args[1].str, args[2].str) == 0) {
+		printf("%s moved to %s\n", args[1].str, args[2].str);
+	} else {
+		perror("error");
+	}
 }
